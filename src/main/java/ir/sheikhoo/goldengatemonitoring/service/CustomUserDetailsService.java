2dcd1fba648b723528b00b3a11460c84a35cb0a7
Mohammad Sadegh Sheikh Zahedi <sheikhoo.iran@gmail.com>
2022-12-12 21:59:28 +0330

    CustomUserDetailsService

---
+++ src/main/java/ir/sheikhoo/goldengatemonitoring/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package ir.sheikhoo.goldengatemonitoring.service;
+
+import ir.sheikhoo.goldengatemonitoring.model.User;
+import ir.sheikhoo.goldengatemonitoring.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(usernameOrEmail);
+        if(user != null){
+            return new org.springframework.security.core.userdetails.User(user.getEmail()
+                    , user.getPassword(),
+                    user.getRoles().stream()
+                            .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                            .toList());
+        }else {
+            throw new UsernameNotFoundException("Invalid email or password");
+        }
+    }
+}
