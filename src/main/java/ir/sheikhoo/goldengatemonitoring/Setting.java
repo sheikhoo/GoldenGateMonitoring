f8d3553bfb62b991436ba694e8b335a6871d65eb
Mohammad Sadegh Sheikh Zahedi <sheikhoo.iran@gmail.com>
2022-12-14 09:31:07 +0330

    don't need Setting class

--- src/main/java/ir/sheikhoo/goldengatemonitoring/Setting.java
+++
@@ -1,9 +0,0 @@
-package ir.sheikhoo.goldengatemonitoring;
-
-public class Setting {
-    // Like /u01/app/ogg
-    public static String GGS_HOME="/u01/app/ogg";
-    public static String GGS_USER="oracleuser";
-    public static String GGS_USER_PWD="****";
-
-}
