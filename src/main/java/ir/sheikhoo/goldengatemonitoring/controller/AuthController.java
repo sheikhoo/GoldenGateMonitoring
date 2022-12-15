e14fb80f040fed2587f8cb1ed175637ac8605a2c
Mohammad Sadegh Sheikh Zahedi <sheikhoo.iran@gmail.com>
2022-12-15 09:50:33 +0330

    add new admin user for first config

--- src/main/java/ir/sheikhoo/goldengatemonitoring/controller/AuthController.java
+++ src/main/java/ir/sheikhoo/goldengatemonitoring/controller/AuthController.java
@@ -2,23 +2,26 @@ package ir.sheikhoo.goldengatemonitoring.controller;
 
 import ir.sheikhoo.goldengatemonitoring.model.User;
 import ir.sheikhoo.goldengatemonitoring.model.UserDto;
+import ir.sheikhoo.goldengatemonitoring.service.ConfigService;
 import ir.sheikhoo.goldengatemonitoring.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class AuthController {
-    private UserService userService;
+    private final UserService userService;
+    private final ConfigService configService;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, ConfigService configService) {
         this.userService = userService;
+        this.configService = configService;
     }
 
     @GetMapping("/admin/user")
@@ -47,6 +50,29 @@ public class AuthController {
 
         userService.saveUser(userDto);
         return "redirect:/admin/user?success";
+    }
+
+    @PostMapping("/admin/user/add")
+    public @ResponseBody Map<String, Boolean> addUser(@RequestBody UserDto userDto){
+        if(!configService.isConfigOk()) {
+            User existingUser = userService.findUserByEmail(userDto.getEmail());
+
+            if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
+                return Collections.singletonMap("success", false);
+            }
+            if(userDto.getEmail()==null || userDto.getPassword()==null){
+                return Collections.singletonMap("success", false);
+            }
+
+            try {
+                userService.saveUser(userDto);
+                return Collections.singletonMap("success", true);
+            } catch (Exception e) {
+                return Collections.singletonMap("success", false);
+            }
+        }else {
+            return Collections.singletonMap("success", false);
+        }
     }
 
     @GetMapping("/admin/users")
